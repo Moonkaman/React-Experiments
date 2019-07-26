bfd77a3aae97210a9b30d4a8ace16b465fc4a5a4
Moonkaman <nickbatmanglidj@gmail.com>
2019-07-25 20:20:19 -0600

    Add Navbar from bootstrap and package to help nav links work with react bootstrap

--- startwars-redux/src/App.js
+++ startwars-redux/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
 
 import "./App.css";
 
@@ -12,6 +15,23 @@ import CharacterView from "./views/CharacterView";
 function App() {
   return (
     <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>SWAPI App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer exact to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/movies">
+              <Nav.Link>Movies</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/characters">
+              <Nav.Link>Characters</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <Container>
         <Route exact path="/movies" component={MoviesView} />
         <Route exact path="/movies/:id" component={MovieView} />
